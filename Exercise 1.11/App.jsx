f7a9b9c56b52279cc935fcdf97c0147bd73ba270
Temple Dees <156852797+tdees01@users.noreply.github.com>
2025-10-03 12:18:49 -0400

    Exercise 1.11

---
+++ Exercise 1.11/App.jsx
@@ -0,0 +1,49 @@
+//Exercise 1.11
+import { useState } from 'react'
+
+const StatisticLine = (props) => {
+  return (
+    <tr>
+      <td>{props.text} {props.value} {props.suffix || ''}</td>
+    </tr>
+  )
+}
+
+const Statistics = (props) => {
+  return (
+    <div>
+      <h1>statistics</h1>
+      <StatisticLine text={"good: "} value={props.good}/>
+      <StatisticLine text={"neutral: "} value={props.neutral} />
+      <StatisticLine text={"bad: "} value={props.bad} />
+      <StatisticLine text={"all: "} value={props.total} />
+      <StatisticLine text={"average: "} value={(((props.good * props.goodVal) + (props.neutral * props.neutralVal) + (props.bad * props.badVal)) / props.total)}  suffix={' %'}/>
+      <StatisticLine text={"positive: "} value={(((props.good * props.goodVal) / props.total) * 100)} suffix={'%'}/>
+    </div>
+
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  let goodVal = 1.0
+  let neutralVal = 0.0
+  let badVal = -1.0
+  let total = good + neutral + bad
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      {good || neutral || bad ? <Statistics good={good} neutral={neutral} bad={bad} total={total} goodVal={goodVal} neutralVal={neutralVal} badVal={badVal}/> : <h5>no feedback given</h5>}
+    </div>
+  )
+}
+
+export default App
