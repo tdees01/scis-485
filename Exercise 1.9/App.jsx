96b1e732ee5f9547abfca6e4bb9f8cb46f2c470d
Temple Dees <156852797+tdees01@users.noreply.github.com>
2025-10-03 10:34:07 -0400

    Add files via upload

---
+++ Exercise 1.9/App.jsx
@@ -0,0 +1,41 @@
+//Exercise 1.9
+import { useState } from 'react'
+
+const Statistics = (props) => {
+  return (
+    <div>
+      <h1>statistics</h1>
+      <p>good: {props.good}</p>
+      <p>neutral: {props.neutral}</p>
+      <p>bad: {props.bad}</p>
+      <p>all: {props.good + props.neutral + props.bad}</p>
+      <p>average: {(((props.good * props.goodVal) + (props.neutral * props.neutralVal) + (props.bad * props.badVal)) / props.total)}%</p>
+      <p>positive: {(((props.good * props.goodVal) / props.total) * 100)}%</p>
+    </div>
+
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  let goodVal = 1.0
+  let neutralVal = 0.0
+  let badVal = -1.0
+  let total = good + neutral + bad
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      {good || neutral || bad ? <Statistics good={good} neutral={neutral} bad={bad} total={total} goodVal={goodVal} neutralVal={neutralVal} badVal={badVal}/> : <h5>no feedback given</h5>}
+    </div>
+  )
+}
+
+export default App
