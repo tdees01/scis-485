da34d4ce5eb0b7056a51d673591d86411ab9dced
Temple Dees <156852797+tdees01@users.noreply.github.com>
2025-10-31 22:01:52 -0400

    Add files via upload

---
+++ exercise2.3/App.jsx
@@ -0,0 +1,67 @@
+
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'Reduxt',
+        exercises: 11,
+        id: 4
+      }
+    ]
+  }
+
+  const Course = (props) => {
+    
+    const Header = (props) => {
+      return (<h1>{props.name}</h1>)
+    }
+    const Part = (props) => {
+      return (
+          <p>{props.part} {props.exercises}</p>
+      )
+    }
+    const Content = (props) => {
+      return (
+        <div>
+          {props.parts.map(part => (
+            <Part key={part.id} part={part.name} exercises={part.exercises} />
+          ))}
+        </div>
+      )
+    }
+
+    return (
+      <div>
+        <Header name={props.course.name} />
+        <Content parts={props.course.parts} />
+        <p>total of {course.parts.reduce((sum, part) => sum + part.exercises, 0)}</p>
+      </div>
+    )
+  }
+  return (
+    <>
+    <Course course={course} />
+    </>
+
+  )
+}
+
+export default App
